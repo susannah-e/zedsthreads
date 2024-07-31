50364ba1cf02df547c7b23657cd24278ac3cb2ed
susannah-e <126836394+susannah-e@users.noreply.github.com>
2024-07-31 21:35:33 +0000

    connected flask app to my database. added index html that website will 'present' by default.

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
 
-app = Flask(__name__)
+DATABASE = "zeds_threads.db"
+
+# Create a flask app
+app = Flask(
+  __name__,
+  template_folder='templates',
+
+)
+def create_connection(zeds_threads):
+  try:
+    connection = sqlite3.connect(zeds_threads)
+    return connection
+  except Error as e:
+    print(e)
+  return None
 
 
+# Index page (now using the index.html file)
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+  return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+  # Run the Flask app
+  app.run(
+  host='0.0.0.0',
+  debug=True,
+  port=8080
+  )
