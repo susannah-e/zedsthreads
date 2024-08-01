1a9e2108e927923f6609081d0091c2707faba21b
susannah-e <126836394+susannah-e@users.noreply.github.com>
2024-08-01 02:46:49 +0000

    got the database displaying on the index page as a test to make sure i could do it

--- main.py
+++ main.py
@@ -4,30 +4,33 @@ from sqlite3 import Error
 
 DATABASE = "zeds_threads.db"
 
-# Create a flask app
-app = Flask(
-  __name__,
-  template_folder='templates',
+# Create a Flask app
+app = Flask(__name__, template_folder='templates')
 
-)
-def create_connection(zeds_threads):
-  try:
-    connection = sqlite3.connect(zeds_threads)
-    return connection
-  except Error as e:
-    print(e)
-  return None
+def create_connection(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
 
-
-# Index page (now using the index.html file)
 @app.route('/')
 def index():
-  return render_template('index.html')
+    con = create_connection(DATABASE)
+    if con:
+        query = "SELECT name, main_colours, price, clothing_catergory FROM clothing_data"
+        cur = con.cursor()
+        cur.execute(query)
+        clothing_list = cur.fetchall()
+        con.close()
+        print(clothing_list)
+        for clothing in clothing_list:
+            print(clothing[3])
+            print(clothing)
+        return render_template('index.html', clothing=clothing_list)
+    else:
+        return "Error"
 
 if __name__ == '__main__':
-  # Run the Flask app
-  app.run(
-  host='0.0.0.0',
-  debug=True,
-  port=8080
-  )
+    app.run(host='0.0.0.0', debug=True, port=8080)
