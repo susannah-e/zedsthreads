458951b6570e0e6f1881b5ebb3c6c5690ed88e38
susannah-e <126836394+susannah-e@users.noreply.github.com>
2024-09-17 23:57:52 +0000

    implemented password stuff dosent work yet

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+from flask import Flask, render_template, redirect, request
 import sqlite3
 from sqlite3 import Error
-
-from flask import Flask, redirect, render_template, request
-
-DATABASE = "zeds_threads.db"
+from flask_bcrypt import bcrypt
+from flask import session
 
 # Create a Flask app
 app = Flask(__name__, template_folder='templates')
+
+DATABASE = "zeds_threads.db"
 
 def create_connection(db_file):
     try:
@@ -37,14 +38,59 @@ def render_database():
         return "Error"
 
 
-@app.route('/login.html', methods = ['POST', 'GET'])
-def login():
+@app.route('/login', methods = ['POST', 'GET'])
+def render_login_page():
+  if is_logged_in():
+    return redirect('/products/1')
+  print("Logging in")
+  if request.method == 'POST':
+    print(request.form)
+    email = request.form['email'].strip().lower()
+    password = request.form['password'].strip()
+    print(email)
+    query = "SELECT id, fname, password FROM user WHERE email =?"
+
+    con = create_connection(DATABASE)
+    cur = con.cursor()
+    cur.execute(query, (email,))
+    user_data = cur.fetchone() #only one value
+    con.close()
+    #if the given email is not in the database it will raise an error
+    if user_data is None:
+      return redirect("/login?error=Email+invalid+or+password+incorrect")
+      return render_template("/login?error=Email+invalid+or+password+incorrect")
+
+
+    try:
+      user_id = user_data[0]
+      first_name = user_data[1]
+      db_password = user_data[2]
+    except IndexError:
+      return redirect("/login?error=Email+invalid+or+password+incorrect")
+
+    if not bcrypt.check_password_hash(db_password, password):
+      return redirect(request.referrer + "?error=Email+invalid+or+password+incorrect")
+
+    session['email'] = email
+    session['user_id'] = user_id
+    session['firstname'] = first_name
+
+    print(session)
+    return redirect('/')
+
+  return render_template('login.html',logged_in = is_logged_in())
+
+@app.route('/logout') #logout function
+def logout():
+  print(list(session.keys()))
+  [session.pop(key) for key in list(session.keys())]
+  print(list(session.keys()))
+  return redirect('/message=See+you+next+time!')
   return render_template('login.html')
-        
-@app.route('/signup.html', methods = ['POST', 'GET'])
+
+
+@app.route('/signup', methods = ['POST', 'GET'])
 def render_signup_page():
-  con = create_connection(DATABASE)
-  cur = con.cursor()
   if request.method == 'POST':
     print(request.form)
     fname = request.form.get('fname').title().strip()
@@ -58,10 +104,14 @@ def render_signup_page():
 
     if len(password) < 8:
       return redirect("/signup?error=Password+must+be+at+least+8+characters")
+    hashed_password = bcrypt.generate_password_hash(password)  #creating a hash password
+    print(hashed_password)  
+    con = create_connection(DATABASE)
     query = "INSERT INTO user(fname, lname, email, password) VALUES(?, ?, ?, ?)"
+    cur = con.cursor()
 
     try:
-      cur.execute(query, (fname, lname, email, password)) #this line actually executes the query
+      cur.execute(query, (fname, lname, email, hashed_password)) #this line actually executes the query
     except sqlite3.IntegrityError:
       con.close()
       return redirect('/signup?error=Email+is+already+used')
@@ -69,11 +119,16 @@ def render_signup_page():
     con.commit()  
     con.close()
 
-    #return redirect("/login")
-  return redirect("/login")
+    return redirect("/login")
+  return render_template('signup.html')
 
-
-    
+def is_logged_in():
+  if session.get("email") is None:
+    print("not logged in")
+    return False
+  else:
+    print("logged in")
+    return True
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5000)
