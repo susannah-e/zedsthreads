f72242f93217dbc086ac8c6a96f50d0b690ce0e1
susannah-e <126836394+susannah-e@users.noreply.github.com>
2024-09-24 08:18:02 +0000

    admin ACTUALLY works...

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, redirect, request
 import sqlite3
 from sqlite3 import Error
-from flask.sansio.scaffold import T_route
 from flask_bcrypt import Bcrypt
 from flask import session
 
@@ -22,7 +21,7 @@ def create_connection(db_file):
 @app.route('/')
 def render_homepage():
   return render_template('index.html')
-  
+
 @app.route('/shopnow.html')
 def render_database():
     con = create_connection(DATABASE)
@@ -32,13 +31,11 @@ def render_database():
         cur.execute(query)
         clothing_list = cur.fetchall()
         con.close()
-      
-        for clothing in clothing_list:
-            print(clothing)  
+
         return render_template('shopnow.html', clothing=clothing_list)
     else:
         return "Error"
-      
+
 @app.route('/outerwear.html')
 def render_outerwear():
     con = create_connection(DATABASE)
@@ -49,13 +46,10 @@ def render_outerwear():
         clothing_list = cur.fetchall()
         con.close()
 
-        if clothing_list:
-            return render_template('outerwear.html', clothing=clothing_list)
-        else:
-            return render_template('outerwear.html', clothing=[], message="No outerwear items found.")
+        return render_template('outerwear.html', clothing=clothing_list)
     else:
         return "Error"
-      
+
 @app.route('/tops.html')
 def render_tops():
     con = create_connection(DATABASE)
@@ -66,10 +60,7 @@ def render_tops():
         clothing_list = cur.fetchall()
         con.close()
 
-        if clothing_list:
-            return render_template('tops.html', clothing=clothing_list)
-        else:
-            return render_template('tops.html', clothing=[], message="No top items found.")
+        return render_template('tops.html', clothing=clothing_list)
     else:
         return "Error"
 
@@ -83,13 +74,10 @@ def render_dresses():
         clothing_list = cur.fetchall()
         con.close()
 
-        if clothing_list:
-            return render_template('dresses.html', clothing=clothing_list)
-        else:
-            return render_template('dresses.html', clothing=[], message="No dresses found.")
+        return render_template('dresses.html', clothing=clothing_list)
     else:
         return "Error"
-      
+
 @app.route('/footwear.html')
 def render_footwear():
     con = create_connection(DATABASE)
@@ -100,13 +88,10 @@ def render_footwear():
         clothing_list = cur.fetchall()
         con.close()
 
-        if clothing_list:
-            return render_template('footwear.html', clothing=clothing_list)
-        else:
-            return render_template('footwear.html', clothing=[], message="No footwear items found.")
+        return render_template('footwear.html', clothing=clothing_list)
     else:
         return "Error"
-      
+
 @app.route('/bottoms.html')
 def render_bottoms():
     con = create_connection(DATABASE)
@@ -117,13 +102,10 @@ def render_bottoms():
         clothing_list = cur.fetchall()
         con.close()
 
-        if clothing_list:
-            return render_template('bottoms.html', clothing=clothing_list)
-        else:
-            return render_template('bottoms.html', clothing=[], message="No bottoms found.")
+        return render_template('bottoms.html', clothing=clothing_list)
     else:
         return "Error"
-      
+
 @app.route('/acessories.html')
 def render_acessories():
     con = create_connection(DATABASE)
@@ -134,10 +116,7 @@ def render_acessories():
         clothing_list = cur.fetchall()
         con.close()
 
-        if clothing_list:
-            return render_template('acessories.html', clothing=clothing_list)
-        else:
-            return render_template('acessories.html', clothing=[], message="No acessories found.")
+        return render_template('acessories.html', clothing=clothing_list)
     else:
         return "Error"
 
@@ -146,20 +125,17 @@ def render_acessories():
 def render_login_page():
     con = create_connection(DATABASE)
     if con:
-        # Check if the user is already logged in
         if is_logged_in():
             first_name = session.get('firstname', 'User')
             return redirect(f'/welcome?name={first_name}')  
 
-        # Handle login POST request
         if request.method == 'POST':
-            email = request.form.get('email').strip().lower()  # make sure email is lowercase
+            email = request.form.get('email')
             password = request.form.get('password')
 
             if not email or not password:
                 return redirect("/login.html?error=Missing+email+or+password")
 
-            # Query to get user by email
             query = "SELECT id, first_name, password FROM user WHERE email = ?"
             cur = con.cursor()
             cur.execute(query, (email,))
@@ -170,41 +146,34 @@ def render_login_page():
 
             user_id = user_data[0]
             first_name = user_data[1]
-            db_password = user_data[2]  #  hashed password from the database
+            db_password = user_data[2]  
 
             if not bcrypt.check_password_hash(db_password, password):
                 return redirect("/login.html?error=Email+invalid+or+password+incorrect")
 
-            # Set session data
             session['email'] = email
             session['user_id'] = user_id
             session['firstname'] = first_name
 
-            return redirect(f'/welcome?name={first_name}')  # redirect to welcome page 
+            return redirect(f'/welcome?name={first_name}')  
 
-        # If GET request, show login page
         error = request.args.get('error')
         return render_template('login.html', logged_in=is_logged_in(), error=error)
     else:
         return "Error connecting to the database."
 
 
-
-
-#chat gpt assisted me with this idea of a welcome page
 @app.route('/welcome')
 def welcome():
-    first_name = request.args.get('name', 'Guest')  # Get the first name from query parameters
+    first_name = request.args.get('name', 'Guest') 
     return render_template('welcome.html', name=first_name)
 
 
-@app.route('/logout') #logout function
+@app.route('/logout')
 def logout():
-  print(list(session.keys()))
   [session.pop(key) for key in list(session.keys())]
-  print(list(session.keys()))
   return render_template('login.html')
-  
+
 @app.route('/signup', methods = ['POST', 'GET'])
 def render_signup_page():
   con = create_connection(DATABASE)
@@ -215,71 +184,79 @@ def render_signup_page():
       email = request.form.get('email')
       password = request.form.get('password')
       password2 = request.form.get('password2')
-  
-      hashed_password = bcrypt.generate_password_hash(password)  #creating a hash password
-      print(hashed_password)  
-      
+
+      hashed_password = bcrypt.generate_password_hash(password)  
+
       query = "INSERT INTO user(first_name, last_name, email, password) VALUES(?, ?, ?, ?)"
       cur = con.cursor()
-  
+
       try:
-        cur.execute(query, (fname, lname, email, hashed_password)) #execute the query
+        cur.execute(query, (fname, lname, email, hashed_password)) 
       except sqlite3.IntegrityError:
         con.close()
         return redirect('/signup?error=Email+is+already+used')
-  
+
       con.commit()  
       con.close()
-  
+
       return redirect("/login.html")
     return render_template('signup.html')
   else:
     return "Error"
 
 def is_logged_in():
-  if session.get("email") is None:
-    print("not logged in")
-    return False
-  else:
-    print("logged in")
-    return True
-    
-#admin section
+  return session.get("email") is not None
+
 @app.route('/admin')
 def render_admin():
     if not is_logged_in():
-        return redirect('/message=Need+to+be+logged+in.')
+        return redirect('/login.html?message=Need+to+be+logged+in.')
     con = create_connection(DATABASE)
     if con:
-        #fetch the categories
-        query = "SELECT * FROM clothing_catergory"
+        query = "SELECT * FROM clothing_category"
         cur = con.cursor()
         cur.execute(query)
         category_list = cur.fetchall()
-    
-        #fetch the products
+
         query = "SELECT * FROM clothing_data"
         cur.execute(query)
         product_list = cur.fetchall()
-        print(product_list)
-    
+
         con.close()
-    
+
         if not product_list:
           return render_template("admin.html", logged_in=is_logged_in(), categories=category_list, no_items=True)
         return render_template("admin.html", logged_in=is_logged_in(), categories=category_list, products=product_list)
+        
+#adding a category function
+@app.route('/add_category', methods = ['POST'])
+def add_category():
+  if not is_logged_in():
+    return redirect('/message=Need+to+be+logged+in.')
+  if request.method == "POST":
+    print(request.form)
+    cat_name = request.form.get('name')
+    print(cat_name)
+    con = create_connection(DATABASE)
+    if con:
+        query = "INSERT INTO clothing_category ('catergory') VALUES (?)"
+        cur = con.cursor()
+        cur.execute(query, (cat_name, ))
+        con.commit()
+        con.close()
+        return redirect('/admin')
 
 
 @app.route('/add_item', methods=['POST'])
 def render_add_item():
     if not is_logged_in():
-        return redirect('/message=Need+to+be+logged+in.')
+        return redirect('/login.html?message=Need+to+be+logged+in.')
 
     if request.method == "POST":
         item_name = request.form.get('name')
         item_colours = request.form.get('main_colours')
         item_price = request.form.get('price')
-        cat_id = request.form.get('cat_id')  # Get the category ID from the form
+        cat_id = request.form.get('cat_id')  
 
         con = create_connection(DATABASE)
         if con:
@@ -291,76 +268,60 @@ def render_add_item():
 
         return redirect('/admin')
 
-#deleting a category function
-@app.route('/delete_category', methods = ['POST'])
-def render_delete_category():
-  if not is_logged_in():
-    return redirect('/message=Need+to+be+logged+in.')
 
-  if request.method == "POST":
+@app.route('/delete_category', methods=['POST'])
+def render_delete_category():
+    if not is_logged_in():
+        return redirect('/login.html?message=Need+to+be+logged+in.')
+
     con = create_connection(DATABASE)
     if con:
-      category = request.form.get('cat_id')
-      print(category)
-      category = category.split(", ")
-      cat_id = category[0]
-      cat_name = category[1]
-      return render_template("delete_confirm.html", id=cat_id, name=cat_name, type='category')
+        cat_id = request.form.get('cat_id')
+        return render_template("delete_confirm.html", id=cat_id, type='category')
     return redirect("/admin")
 
-#confirmation of delete category
+
 @app.route('/delete_category_confirm/<int:cat_id>')
 def render_delete_category_confirm(cat_id):
-  if not is_logged_in():
-    return redirect('/message=Need+to+be+logged+in.')
-  con = create_connection(DATABASE)
-  if con:
-    query = "DELETE FROM clothing_catergory WHERE id = ?"
-    cur = con.cursor()
-    cur.execute(query, (cat_id, ))
-    con.commit()
-    con.close()
+    if not is_logged_in():
+        return redirect('/login.html?message=Need+to+be+logged+in.')
+
+    con = create_connection(DATABASE)
+    if con:
+        query = "DELETE FROM clothing_category WHERE catergoryid = ?"
+        cur = con.cursor()
+        cur.execute(query, (cat_id, ))
+        con.commit()
+        con.close()
     return redirect("/admin")
 
 
-#deleting a item function
 @app.route('/delete_item', methods=['POST'])
 def render_delete_item():
-  if not is_logged_in():
-    return redirect('/message=Need+to+be+logged+in.')
+    if not is_logged_in():
+        return redirect('/login.html?message=Need+to+be+logged+in.')
 
-  if request.method == "POST":
     con=create_connection(DATABASE)
-    products = request.form.get('clothing_id')
-    print(products)
-    if products is None:
-      return redirect("/admin?error=No+item+selected")
+    if con:
+        item_id = request.form.get('item_id')
+        return render_template("delete_item_confirm.html", id=item_id, type='item')
+    return redirect("/admin")
 
-    products = products.split(", ")
-    item_id = products[0]
-    item_name = products[1] if len(products) > 1 else "" #assign an empty string if item_name doesn't exist
-    print(item_id, item_name)
 
-    return render_template("delete_item_confirm.html", id=item_id, name=item_name, type='products')
-  return redirect("/admin")
-
-#confirm delete item
 @app.route('/delete_item_confirm/<int:item_id>')
 def render_delete_item_confirm(item_id):
-  print("I am in here")
-  if not is_logged_in():
-    return redirect('/message=Need+to+be+logged+in.')
+    if not is_logged_in():
+        return redirect('/login.html?message=Need+to+be+logged+in.')
 
-  con=create_connection(DATABASE)
-  if con:
-    query = "DELETE FROM clothing_data WHERE id = ?"
-    cur = con.cursor()
-    cur.execute(query, (item_id, ))
-    con.commit()
-    print("Test: ", item_id)
-    con.close()  
+    con=create_connection(DATABASE)
+    if con:
+        query = "DELETE FROM clothing_data WHERE id = ?"
+        cur = con.cursor()
+        cur.execute(query, (item_id, ))
+        con.commit()
+        con.close()
+    return redirect("/admin")
 
-  return redirect("/admin")
-  
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True, port=8080)
+
+if __name__ == "__main__":
+    app.run(debug=True)
